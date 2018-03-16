10c382251726ab4f719569394d9cbb0fa7b9d7dd
João <Joo@JCFM>
2018-03-16 17:27:55 +0000

    Teste inicial

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO!");
+		System.out.println("This app show sales data");
+	}
+
+}
